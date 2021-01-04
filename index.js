0d0358173002ebdfc48afd5695ce07629838bac0
Duc Nguyen Minh <duc.nguyen-minh@tu-ilmenau.de>
2021-01-04 14:45:05 +0100

    refactore code

--- index.js
+++ index.js
@@ -3,36 +3,19 @@ var myWorkPosition = $(".myWork-box").offset().top;
 var contactPosition = $(".contact-box").offset().top;
 var descriptionPosition = ($(".description-box").offset().top - $(".description-box").outerHeight())
 
+function textAnimation (scrollpoint, target, toggleClass) {
+  if ($(document).scrollTop() > scrollpoint) {
+    $(target).addClass(toggleClass);
+  } else if ($(document).scrollTop() < scrollpoint)
+    $(target).removeClass(toggleClass);
+}
+
 $(document).scroll(function() {
-  navBar();
-  scrollingText();
-  scrollingText2();
+  textAnimation (90, ".navbar", "navigation-background");
+  textAnimation (300, ".description-box", "scrolling-text");
+  textAnimation (1100, ".myWorkLeft", "scrolling-text");
 });
 
-function navBar() {
-
-  if ($(document).scrollTop() > 70) {
-    $(".navbar").addClass("navigation-background");
-  } else if ($(document).scrollTop() < 70)
-    $(".navbar").removeClass("navigation-background");
-}
-
-function scrollingText() {
-  if ($(document).scrollTop() > 200) {
-    $(".description-box").addClass("scrolling-text");
-  } else if ($(document).scrollTop() < 200) {
-    $(".description-box").removeClass("scrolling-text");
-  }
-}
-
-function scrollingText2() {
-  if ($(document).scrollTop() > 900) {
-    $(".myWorkLeft").addClass("scrolling-text");
-    console.log("go");
-  } else if ($(document).scrollTop() < 900) {
-    $(".myWorkLeft").removeClass("scrolling-text");
-  }
-}
 
 // $(".nav-link").click(function(event){
 //   buttonClick(event.target.id);
