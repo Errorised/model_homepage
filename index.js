ae9cc3fcec810849977bc1fcaffccedb8c2a30ed
Duc Nguyen Minh <duc.nguyen-minh@tu-ilmenau.de>
2021-01-22 18:51:46 +0100

    add resize function to handle animations per screensize

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ var myWorkPosition = $(".myWork-box").offset().top;
 var contactPosition = $(".contact-box").offset().top;
 var descriptionPosition = ($(".description-box").offset().top - $(".description-box").outerHeight())
 
-function textAnimation (scrollpoint, target, toggleClass) {
+function textAnimation(scrollpoint, target, toggleClass) {
   if ($(document).scrollTop() > scrollpoint) {
     $(target).addClass(toggleClass);
   } else if ($(document).scrollTop() < scrollpoint)
     $(target).removeClass(toggleClass);
 }
 
+$(window).on("resize", () => {
+  console.log($(document).width) ;
+  if ($(document).width() > 990) {
+    $(document).scroll(function () {
+      textAnimation(100, ".navbar", "navigation-background");
+      textAnimation(450, ".description-box", "scrolling-text");
+      textAnimation(800, ".myWorkLeft", "scrolling-text");
+    });
+  }
+  
+  else if ($(document).width() < 990 && $(document).width() > 576) {
+    console.log("990")
+    $(document).scroll(function () {
+      textAnimation(20, ".navbar", "navigation-background");
+      textAnimation(300, ".description-box", "scrolling-text");
+      textAnimation(600, ".myWorkLeft", "scrolling-text");
+    });
+  } else if ($(document).width() < 576) {
+    $(".navbar").addClass("navigation-background")
+    $(".description-box").addClass("scrolling-text");
+    $(document).scroll(function () {
+      textAnimation(100, ".myWorkLeft", "scrolling-text");
+    });
+  }
+  
+}).resize();
 
-if ($(document).width() < 990 && $(document).width() > 576){
-  console.log("990")
-  $(document).scroll(function() {
-    textAnimation (20, ".navbar", "navigation-background");
-    textAnimation (300, ".description-box", "scrolling-text");
-    textAnimation (600, ".myWorkLeft", "scrolling-text");
-  });
-} else if ($(document).width() < 576 ) {
-  $(".navbar").addClass("navigation-background")
-  $(".description-box").addClass("scrolling-text");
-  $(document).scroll(function() {
-    textAnimation (100, ".myWorkLeft", "scrolling-text");
-  });
-}
+
+
+
+
 
 
 
